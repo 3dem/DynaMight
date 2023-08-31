9b054c9f47738f243f8a23b066be6eaefd4e5afa
schwabjohannes <schwaebou@gmail.com>
2023-08-31 17:33:25 +0100

    Update optimize_inverse_deformations.py

--- dynamight/inverse_deformations/optimize_inverse_deformations.py
+++ dynamight/inverse_deformations/optimize_inverse_deformations.py
@@ -46,7 +46,7 @@ def optimize_inverse_deformations(
     if checkpoint_file is None:
         checkpoint_file = forward_deformations_directory / 'checkpoint_final.pth'
 
-    checkpoint = torch.load(vae_checkpoint, map_location=device)
+    checkpoint = torch.load(checkpoint_file, map_location=device)
 
     if refinement_star_file == None:
         refinement_star_file = checkpoint['refinement_directory']
